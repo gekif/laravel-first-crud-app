5319f66f68076bc79372f6a7b5d5c16b5b69ea3b
Dzulfikar Maulana <dzulfikar.maulana@gmail.com>
2020-02-27 11:31:52 +0700

    Fix faker on first name and last name

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -16,8 +16,8 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Contact::class, function (Faker $faker) {
     return [
-        'first_name' => $faker->name,
-        'last_name' => $faker->name,
+        'first_name' => $faker->firstName,
+        'last_name' => $faker->lastName,
         'email' => $faker->email,
         'job_title' => $faker->jobTitle,
         'city' => $faker->city,
